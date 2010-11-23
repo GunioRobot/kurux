fcf3f67fd90bfaca15720b0f279dda1ff04a3a10
Albert Kurucz <albert.kurucz@gmail.com>
2010-11-23 08:55:44 -0600

    add hello

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QtCore/QCoreApplication>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    QTextStream cout(stdout, QIODevice::WriteOnly);
+    cout << "hello" << endl;
     return a.exec();
 }
