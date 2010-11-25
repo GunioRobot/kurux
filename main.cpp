511d3930a6e895315137c871c710521a0e27f05a
ali65 <albert.kurucz@gmail.com>
2010-11-25 00:08:35 -0600

    return error code add LGPL header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
-#include <QtCore/QCoreApplication>
+/*
+
+Copyright (c) 2010 Albert Kurucz.
+
+This file, main.c is part of Kurux.
+
+Kurux is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kurux is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Kurux.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+//#include <QtCore/QCoreApplication>
 #include <QTextStream>
 
 void foo(void){
@@ -7,7 +28,8 @@ void foo(void){
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
+    //a.exec();
     QTextStream cout(stdout, QIODevice::WriteOnly);
     QTextStream cerr(stderr, QIODevice::WriteOnly);
     cout << "hello world" << endl;
@@ -15,7 +37,7 @@ int main(int argc, char *argv[])
         foo();
     }catch(const char* errorMessage) {
         cerr << errorMessage << endl;
-        a.exit(1);
-    }
-    return a.exec();
+        return 1;
+    }    
+    cout << "end" << endl;
 }
