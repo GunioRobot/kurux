239bdc32958d055a579eda625bbb6e6f166b4015
ali65 <albert.kurucz@gmail.com>
2010-11-24 23:21:10 -0600

    add some exception handling

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <QtCore/QCoreApplication>
 #include <QTextStream>
 
+void foo(void){
+    throw "Error in foo";
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QTextStream cout(stdout, QIODevice::WriteOnly);
+    QTextStream cerr(stderr, QIODevice::WriteOnly);
     cout << "hello world" << endl;
+    try{
+        foo();
+    }catch(const char* errorMessage) {
+        cerr << errorMessage << endl;
+        a.exit(1);
+    }
     return a.exec();
 }
